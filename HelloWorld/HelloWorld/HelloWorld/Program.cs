63f537104dba45fe0abb14ffd04a563d02441115
Tranzite <54800477+Tranzite@users.noreply.github.com>
2020-09-11 14:21:30 +0200

    listor och arrayer

---
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"David","Niklas","Jacob","Gabbe","Andreas"};
+
+            int i = 4;
+            while (i < names.Length && i > -1){
+                Console.WriteLine(names[i]);
+                i--;
+            }
+        }
+    }
+}
